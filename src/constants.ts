7f3d4017f868a707fa22ed1e9dabe4645ea231c2
Chance Hudson <jchancehud@gmail.com>
2020-09-30 21:39:27 -0500

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -22,9 +22,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
